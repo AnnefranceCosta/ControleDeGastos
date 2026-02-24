7659b0526ce2237cdaf34e5e334d7781dda20f8a
Annefrance Costa <annefrance.costa@gmail.com>
2026-02-24 17:21:38 -0300

    FormasPagamento model criacao

---
+++ Models/FormasPagamento.cs
@@ -0,0 +1,8 @@
+namespace ControleDeGastos.Models
+{
+    public class FormasPagamento
+    {
+        public int FormasPagamentoId { get; set; }
+        public string NomeFormaPagamento { get; set; }
+    }
+}
